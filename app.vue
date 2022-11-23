719b5a437e2b253a7518b65d358a4ae377148924
Sabrina <sabrinath1@hotmail.com>
2022-11-23 18:30:45 -0300

    created paste public

--- app.vue
+++ app.vue
@@ -1,6 +1,4 @@
 <template>
-  <MoleculesHeader></MoleculesHeader>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <input type="radio" name="radio-1" class="radio" checked />
-  <input type="radio" name="radio-1" class="radio" />
+  <MoleculesPresentation></MoleculesPresentation>
+  <MoleculesFooter></MoleculesFooter>
 </template>
