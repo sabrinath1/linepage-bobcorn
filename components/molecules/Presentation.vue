07b80a1ec20eb555cba2ae03104967b21e360a9f
Sabrina <sabrinath1@hotmail.com>
2022-11-23 18:28:39 -0300

    create presetation main

---
+++ components/molecules/Presentation.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <img
+        src="/img/bob.png"
+        alt="Logo"
+        class="max-w-sm rounded-lg shadow-2xl"
+      />
+      <div>
+        <h1 class="text-5xl font-bold">Foi Mal!</h1>
+        <p class="py-6">
+          O Bob estpa preparando algo incrível e por isso nossa lojinha está em
+          manutenção.
+        </p>
+
+        <p class="py-6">
+          Mas fique tranquilo, você pode conferir nossos produtos em nosso
+          Instragram!
+        </p>
+        <button class="btn btn-primary">Dar uma espiada</button>
+      </div>
+    </div>
+  </div>
+</template>
