9aef8b1e5b5c8464c4793313e0b71c51f3cd31ec
Sabrina <sabrinath1@hotmail.com>
2022-11-23 18:29:07 -0300

    configuration colors theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,4 +10,15 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+         
+          "primary-focus": "purple",
+        },
+      },
+    ],
+  },
 }
